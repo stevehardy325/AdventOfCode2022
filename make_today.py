02e33ca2cdeeec1b1c4cdc65e4d73d54ff2b3cc1
Steve Hardy <stevehardy325@gmail.com>
2022-12-12 01:31:56 -0500

    further simplifying make_today.py script to generate solution boilerplate

--- make_today.py
+++ make_today.py
@@ -6,13 +6,6 @@ today = dt.date.today()
 day_of_month_int = today.day
 dom_2_digit = '{:02d}'.format(day_of_month_int)
 print(dom_2_digit)
-'''shutil.copyfile('base_example.txt', '{}_example.txt'.format(dom_2_digit))
-shutil.copyfile('base_input.txt', '{}_input.txt'.format(dom_2_digit))
-with open('base_pt1.py') as base_py_fobj:
-    base_py_str = base_py_fobj.read()
-    print(base_py_str)
-    formatted_py_str = base_py_str.format(day_str=dom_2_digit)
-    with open('{}_pt1.py'.format(dom_2_digit), 'w') as new_py_fobj:
-        new_py_fobj.write(formatted_py_str)'''
+
 shutil.copytree('./base/', './{}/'.format(dom_2_digit))
 
