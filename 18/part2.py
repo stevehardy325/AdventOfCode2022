844182ad03b65d5fc11535e615fc2060eac10853
Steve Hardy <stevehardy325@gmail.com>
2022-12-18 01:52:30 -0500

    18th part 2 - using path walking to find outside

--- 18/part2.py
+++ 18/part2.py
@@ -7,70 +7,58 @@ input_filename = 'input.txt'
 
 
 def getAdjacent(x, y, z) -> set:
-    directions = [-1, 1]
     adjacent_cubes = set()
-    for direction_unit in directions:
+    for direction_unit in [-1, 1]:
         adjacent_cubes.add((x+direction_unit,y,z))
         adjacent_cubes.add((x,y+direction_unit,z))
         adjacent_cubes.add((x,y,z+direction_unit))
     return adjacent_cubes
 
-def getBubbles(present, missing):
-    min_y,min_x,min_z,max_y,max_y,max_y = 0,0,0,0,0,0
+def getOutside(present):
+    min_y,min_x,min_z,max_y,max_z,max_x = 0,0,0,0,0,0
 
     for x,y,z in present:
-        if min_x is None or x < min_x:
-            min_x = x -1
-        if max_x is None or x > max_x:
-            max_x = x + 1
-        if min_y is None or y < min_y:
-            min_y = y - 1
-        if max_y is None or y > max_y:
-            max_y = y + 1
-        if min_z is None or z < min_z:
-            min_z = z -1
-        if max_z is None or z > max_z:
-            max_z = z +1
+        min_y = min(min_y, y - 5)
+        min_x = min(min_x, x - 5)
+        min_z = min(min_z, z - 5)
+        max_y = max(max_y, y + 5)
+        max_x = max(max_x, x + 5)
+        max_z = max(max_z, z + 5)
 
-    in_bubble = set()
+    #bounding box
+    all_in_box = set([(x,y,z) for y in range(min_y-1, max_y+1) for z in range(min_z-1, max_z+1) for x in range(min_x-1, max_x+1)])
 
-    directions = [(0,0,1),(0,1,0),(1,0,0),(0,0,-1),(0,-1,0),(-1,0,0)]
-    
-    for node in missing:
-        x,y,z = node
-        hit_count = 0
-        for dx, dy, dz in directions:
-            while y < max_y and y > min_y and z < max_z and z > min_z and x < max_x and x > min_x:
-                x += dx
-                y += dy
-                z += dz
-                if (x,y,z) in present:
-                    hit_count += 1
-                    break
-        if hit_count == 6:
-            in_bubble.add(node)
+    #path-walking to see where we can touch from outside, start at a corner outside of the droplet
+    seen = set()
+    checking = [(max_x,max_y,max_z)]
+    while len(checking) > 0:
+        check_next = set()
+        for x,y,z in checking:
+            seen.add((x,y,z))
+            # looking for unvisited open spaces only
+            adjacent = getAdjacent(x,y,z).difference(seen).difference(present)
 
+            # limit to bounding box so we don't grow infinitely
+            adjacent = adjacent.intersection(all_in_box)
 
-    return in_bubble
+            check_next = check_next.union(adjacent)
+        checking = check_next
 
+    return seen
 
-def getMissingAdjacentNum(x,y,z,present):
+def getMissingAdjacentNum(x,y,z,present, outside):
     adjacent = getAdjacent(x,y,z)
-    #print(x,y,z, adjacent, present)
-    missing = adjacent - present
+    missing = adjacent.difference(present)
+    missing = outside.intersection(missing)
     return len(missing)
 
 def process(fname):
     print('processing...')
-    directions = [-1, 1]
-
     nodes = set([tuple(int(n) for n in l.split(',')) for l in open(fname).read().split('\n')])
-
+    outside = getOutside(nodes)
     missing = 0
     for node in nodes:
-        missing += getMissingAdjacentNum(*node, nodes)
-
-    
+        missing += getMissingAdjacentNum(*node, nodes, outside)
     
     print(missing)
 
